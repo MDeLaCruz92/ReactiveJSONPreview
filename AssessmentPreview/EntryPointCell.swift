d782fdc3cb0a2ad662e0449338f98a645b6b0956
MDeLaCruz92 <delacruzm10@gmail.com>
2017-08-25 02:37:24 -0400

    Refactor the UI and code structure.

---
+++ AssessmentPreview/EntryPointCell.swift
@@ -0,0 +1,24 @@
+//
+//  EntryPointCell.swift
+//  AssessmentPreview
+//
+//  Created by Michael De La Cruz on 8/21/17.
+//  Copyright © 2017 Michael De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class EntryPointCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
